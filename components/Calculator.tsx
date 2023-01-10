ea2204929109529d189cfade0296e65fad74d361
NoahAldhous <naldhous@hotmail.co.uk>
2023-01-10 14:43:53 +0000

    added initial states for calculator component, began writing functions for calculating bill and service charge

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,12 +1,28 @@
+import { number } from 'prop-types';
 import React, {useState} from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
 
 const Calculator = () => {
 
-    const [bill, setBill] = useState(0);
+    const [userInput, setUserInput] = useState<string>("100")
+    
+    const [serviceCharge, setServiceCharge] = useState<number>(10);
+
+    const [bill, setBill] = useState<string>("0")
+
+    const calculateBill = () => {
+        let billWithoutTip = Number(userInput);
+        let tip:number = Number(((billWithoutTip / 100) * serviceCharge).toFixed(2))
+        setBill((billWithoutTip + tip).toFixed(2))
+    }
 
     return <View>
-        <Text>Bill:{bill}</Text>
+        <Text>User Input : {userInput}</Text>
+        <Text>Service Charge : {serviceCharge}</Text>
+        <Text>Bill: {bill}</Text>
+        <Button title="calculate bill" onPress={calculateBill}></Button>
+        <Button title="10%" onPress={() => {setServiceCharge(10)}}></Button>
+        <Button title="20%" onPress={() => {setServiceCharge(20)}}></Button>
     </View>
 };
 
